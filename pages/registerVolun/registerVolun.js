77398f6ce953d0a1e3d3ca484c86ba1f6b21d252
bemzj <qiuzijia520@qq.com>
2018-05-28 12:01:23 +0800

    xx

--- pages/registerVolun/registerVolun.js
+++ pages/registerVolun/registerVolun.js
@@ -1,11 +1,26 @@
 // pages/registerVolun/registerVolun.js
 Page({
-
+  bindRegionChange: function (e) {   
+    var index = e.target.dataset.index;
+    var nowRegion = this.data.region; 
+    nowRegion[index].province = e.detail.value[0];
+    nowRegion[index].city = e.detail.value[1];
+    nowRegion[index].area = e.detail.value[2];
+    this.setData({
+      region:nowRegion
+    })
+  },
   /**
    * 页面的初始数据
    */
   data: {
-  
+    region: [
+      {  
+        'province':"省",
+        'city':"市",
+        'area':"区"
+      }
+    ]
   },
 
   /**
@@ -28,7 +43,52 @@ Page({
   onShow: function () {
   
   },
-
+  addAddress:function(){
+    var nowRegion = this.data.region;
+    nowRegion.push(
+      {
+        'province':"省",
+          'city':"市",
+            'area':"区"
+      }
+    );
+    this.setData({
+      region: nowRegion
+    });
+  },
+  subtractionAddress:function(e){
+    var index = e.currentTarget.dataset.index;
+    var nowRegion = this.data.region;
+    var self = this;
+    if (typeof index == 'undefined')
+    {
+      wx.showToast({
+        title: '操作频繁，删除失败',
+        icon: 'fail',
+        duration: 5000
+      })
+    }else{
+      wx.showModal({
+        title: '微信提示',
+        content: '是否删除该地址',
+        mask: 'true',
+        success: function (res) {
+          if (res.confirm) {
+            nowRegion.splice(index, 1);
+            self.setData({
+              region: nowRegion
+            });
+            wx.showToast({
+              title: '删除成功',
+              icon: 'success',
+              duration: 2000
+            })
+          }
+        }
+      })
+    }
+    
+  },
   /**
    * 生命周期函数--监听页面隐藏
    */
