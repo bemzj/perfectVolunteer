0d058059f4d0747002dbbdb334979ac5806409ef
zhongRui_HE <418351897@qq.com>
2018-05-29 17:34:21 +0800

    123

---
+++ pages/hisComment/hisComment.js
@@ -0,0 +1,71 @@
+// pages/hisComment/hisComment.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    dataList: [
+      { image: '../../images/head01.png', client: '陈小姐', date: '2018.06.02', comment:'杨义工很耐心，棒棒哒！' },
+      { image: '../../images/head01.png', client: '陈小姐', date: '2018.06.02', urge: 0 },
+      { image: '../../images/head01.png', client: '陈小姐', date: '2018.06.02', urge: 0 },
+      { image: '../../images/head01.png', client: '陈小姐', date: '2018.06.02', urge: 0 }
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
