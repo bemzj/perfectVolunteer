4d0dbf1361ccc908948b5226e84c0b457192bc82
bemzj <qiuzijia520@qq.com>
2018-05-30 15:38:41 +0800

    5.30.15.38

--- pages/hisComment/hisComment.js
+++ pages/hisComment/hisComment.js
@@ -6,13 +6,26 @@ Page({
    */
   data: {
     dataList: [
-      { image: '../../images/head01.png', client: '陈小姐', tel: '15999911956', date: '2018.06.02', comment: "",'star':5 ,status:0},
-      { image: '../../images/head01.png', client: '陈小姐', tel: '15999911956', date: '2018.06.02', comment: "", 'star': 4,status: 1},
-      { image: '../../images/head01.png', client: '陈小姐', tel: '15999911956', date: '2018.06.02', comment: "广东金融学院广东金融学院广东金融学院", 'star': 4,status: 0},
-      { image: '../../images/head01.png', client: '陈小姐', tel: '15999911956', date: '2018.06.02', comment: "广东金融学院广东金融学院广东金融学院广东金融学院广东金融学院广东金融学院", 'star': 3,status: 0}
+      { image: '../../images/head01.png', client: '陈小姐1', identifier:'123456',tel: '15999911956', date: '2018.06.02', comment: "",'star':5 ,status:0},
+      { image: '../../images/head01.png', client: '陈小姐2', identifier: '123457',tel: '15999911956', date: '2018.06.02', comment: "", 'star': 4,status: 1},
+      { image: '../../images/head01.png', client: '陈小姐3', identifier: '123458',tel: '15999911956', date: '2018.06.02', comment: "广东金融学院广东金融学院广东金融学院", 'star': 4,status: 0},
+      { image: '../../images/head01.png', client: '陈小姐4', identifier: '123459',tel: '15999911956', date: '2018.06.02', comment: "广东金融学院广东金融学院广东金融学院广东金融学院广东金融学院广东金融学院", 'star': 3,status: 0}
     ],
     index1: null,
-    user_type:'U'
+    user_type:'U',
+    currentVolunteer:{
+      name: '',//姓名
+      identifier: '',//编号
+      src:''  //头像路径  
+    },
+    starLevel: 0,//星星等级
+    commentLevel:["您还没评价","非常不满意","不满意","一般满意","满意","非常满意"],
+    commentShow:false,
+    tipStatus1: false, //弹窗1
+    tipStatus2: false, //弹窗2
+    poptext1:"",
+    commentIndex:-1,
+    commentTxt:''
   },
   tixing: function (e) {
     // let num = e.currentTarget.dataset.index
@@ -84,5 +97,69 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  //选择星星等级
+  selectLevel: function(e){
+    this.setData({
+      starLevel: e.currentTarget.dataset.index
+    });
+  },
+  //显示评论
+  showComment:function(e){   
+    var datas = this.data.dataList[e.currentTarget.dataset.index];
+    var opj = this.data.currentVolunteer;
+    opj.name = datas.client;
+    opj.identifier = datas.identifier;
+    opj.src = datas.image;
+     
+    this.setData({
+      commentShow:!this.data.commentShow,
+      currentVolunteer: opj,
+      starLevel:0,
+      commentIndex: e.currentTarget.dataset.index
+    });
+  },
+  //失去焦点
+  getCommentText:function(e){
+    this.setData({
+      commentTxt: e.detail.value
+    });
+  },
+  //提交评论
+  comfrimComment:function(){
+    this.setData({
+      tipStatus1: !this.data.tipStatus1,
+      poptext1:"感谢您的评论！"
+    });
+  },
+  //关闭页面
+  closeComment:function(){
+    this.setData({
+      commentShow: !this.data.commentShow
+    });
+  },
+  // 弹窗1取消
+  closeTip: function () {
+    //如果提交成功
+    var obj = this.data.dataList;
+    obj[this.data.commentIndex].comment = this.data.commentTxt;
+    obj[this.data.commentIndex].star = this.data.starLevel;
+    this.setData({
+      tipStatus1: !this.data.tipStatus1,
+      commentShow: !this.data.commentShow,
+      dataList: obj
+    });
+  },
+  // 弹窗2取消
+  cancel: function () {
+    this.setData({
+      tipStatus2: !this.data.tipStatus2
+    });
+  },
+  // 弹窗2确定
+  comfirmPop: function () {
+    this.setData({
+      tipStatus2: !this.data.tipStatus2
+    });
+  },
 })
